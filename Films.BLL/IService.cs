69c916b0fd88b0fdac1fc3dab2bb488927847ff8
Starinin Andrey <starinin-andrey@ya.ru>
2025-04-12 21:06:09 +0300

    Добавление в BLL интерфейса IService

---
+++ Films.BLL/IService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Films.Model;
+
+namespace Films.BLL;
+
+public interface IService
+{
+    public bool Create(Film film);
+    public IEnumerable<Film> GetAll();
+    public IEnumerable<Film> GetByTitle(string title);
+}
