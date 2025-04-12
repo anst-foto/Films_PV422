7994965059aca576fb7d62134f14f9cd1dec5891
Starinin Andrey <starinin-andrey@ya.ru>
2025-04-12 21:01:24 +0300

    Добавление проекта Model

---
+++ Films.Model/Film.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Films.Model;
+
+public class Film
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public required DateTime ReleaseDate { get; set; }
+}
