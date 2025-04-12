3906da6e003fa222ec11dc1fd6c175763a0b32da
Starinin Andrey <starinin-andrey@ya.ru>
2025-04-12 21:15:06 +0300

    Реализация в BLL интерфейса IService

---
+++ Films.BLL/Service.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Films.DAL;
+using Films.Model;
+
+namespace Films.BLL;
+
+public class Service : IService
+{
+    private ICrud _crud;
+
+    public Service(ICrud crud)
+    {
+        _crud = crud;
+    }
+
+    public bool Create(Film film)
+    {
+        return _crud.Create(film);
+    }
+
+    public IEnumerable<Film> GetAll()
+    {
+        return _crud.GetAll();
+    }
+
+    public IEnumerable<Film> GetByTitle(string title)
+    {
+        var films = _crud.GetAll();
+        return films.Where(f => f.Title.Contains(title, System.StringComparison.CurrentCultureIgnoreCase));
+    }
+}
