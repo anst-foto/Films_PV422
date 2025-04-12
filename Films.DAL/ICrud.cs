39437a4a397103a7ab898ad1845d7b267acb5ac6
Starinin Andrey <starinin-andrey@ya.ru>
2025-04-12 21:14:40 +0300

    Добавление в DAL интерфейса ICrud

---
+++ Films.DAL/ICrud.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Films.Model;
+
+namespace Films.DAL;
+
+public interface ICrud
+{
+    public bool Create(Film film);
+    public bool Update(Film film);
+    public bool Delete(Guid id);
+
+    public IEnumerable<Film> GetAll();
+}
